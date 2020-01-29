86ebeaf921ee0c47c4ed81994c8bbbb03d39e51f
kyletgithub <kyledthreatt@gmail.com>
2020-01-29 13:04:02 -0600

    initial commit

---
+++ classes/Weapon.java
@@ -0,0 +1,45 @@
+public class Weapon {
+    private int dmg;
+    private String dmgType;
+    private String WEAPON_TYPE;
+    private double crit;
+
+    public Weapon(int dmg, String dmgType, double crit) {
+        this.dmg = dmg;
+        this.dmgType = dmgType;
+        this.crit = crit;
+        WEAPON_TYPE = "Weapon";
+    }
+
+    public int attack(Character target) {
+        int tempDmg = dmg;
+        tempDmg *= Math.random()/2 + .5;
+        for(String weakness : target.weakness) {
+            if (weakness.equals(dmgType)) {
+                return dmg*2;
+            }
+        }
+        
+
+        return dmg;
+    }
+
+    public String getWeaponType() {
+        return WEAPON_TYPE;
+    }
+
+    public String getDmgType() {
+        return dmgType;
+    }
+
+    public int getDmg() {
+        return dmg;
+    }
+
+    public double getCrit() {
+        return crit;
+    }
+
+
+
+}
