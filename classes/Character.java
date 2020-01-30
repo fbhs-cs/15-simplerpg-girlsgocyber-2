5509cd5c7845d1d2653f48d1a599a0acba328f35
notAlexB <alex.billiot@gmail.com>
2020-01-30 13:15:22 -0600

    sto

--- classes/Character.java
+++ classes/Character.java
@@ -10,66 +10,131 @@ class Character{
         speed = speedIn;
     }
 
+    
+    /** 
+     * @return int
+     */
     public int getHP(){
         return this.hp;
     }
 
+    
+    /** 
+     * @param in
+     */
     public void setHP(int in){
         this.hp = in;
     }
 
+    
+    /** 
+     * @param in
+     */
     public void setLevel(int in){
         this.xp = in * in;
     }
 
+    
+    /** 
+     * @param in
+     */
     public void addXP(int in){
         this.xp += in;
     }
 
+    
+    /** 
+     * @return int
+     */
     public int getLevel(){
         return (int) Math.sqrt(xp);
     }
 
+    
+    /** 
+     * @return int
+     */
     private int getMaxXP(){
         return this.getLevel() * 10;
     }
 
+    
+    /** 
+     * @param in
+     * @return boolean
+     */
     public boolean isWeakTo(String in){
         return Arrays.asList(weaknesses).contains(in) ;
     }
 
+    
+    /** 
+     * @return String
+     */
     public String getName(){
         return this.name;
     }
 
+    
+    /** 
+     * @param in
+     */
     public void takeDmg(int in){
         this.hp -= in;
     }
 
+    
+    /** 
+     * @return int
+     */
     public int getMana(){
         return this.mana;
     }
 
+    
+    /** 
+     * @param in
+     */
     public void setMana(int in){
         this.mana = in;
     }
 
+    
+    /** 
+     * @param in
+     */
     public void useMana(int in){
         this.mana -= in;
     }
 
+    
+    /** 
+     * @return boolean
+     */
     public boolean isAlive(){
         return this.hp > 0;
     }
 
+    
+    /** 
+     * @return int
+     */
     public int getSpeed(){
         return this.speed;
     }
 
+    
+    /** 
+     * @param in
+     */
     public void chooseAction(Character in){
 
     }
 
+    
+    /** 
+     * @return String
+     */
     public String toString(){
         return "";
     }
