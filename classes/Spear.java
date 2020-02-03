1cc5009c910649d7d01fa5f84faa8eed6f4bd543
Egor Mikhaylov <egorvtoronto@gmail.com>
2020-02-03 12:51:45 -0600

    working spear

--- classes/Spear.java
+++ classes/Spear.java
@@ -1,6 +1,22 @@
 public class Spear extends Weapon {
-    public Spear(int dmg, String dmgType, double crit) {
-        super(dmg, dmgType, crit);
+    public Spear(int dmg, double crit) {
+        super(dmg, "Piercing", crit);
         WEAPON_TYPE = "Spear";
     }
+
+    public int attack(Character target) {
+        int tempDmg = this.getDmg();
+        tempDmg *= Math.random()/2 + .5;
+        if (target.isWeakTo(this.getDmgType())) {
+            tempDmg *= 2;
+        }
+        double critRoll = Math.random();
+        if (critRoll < this.getCrit()) {
+            System.out.println("You critically hit!");
+            tempDmg *= 2;
+        }
+        System.out.println("You swing your Spear");
+        return tempDmg;
+    }
+    
 }
