b3ba938cf9b5b2185bf00a08c6acb09e7e7787de
Egor Mikhaylov <egorvtoronto@gmail.com>
2020-02-05 12:53:39 -0600

    added wielder

--- classes/Brigand_Spearman.java
+++ classes/Brigand_Spearman.java
@@ -3,6 +3,7 @@ public class Brigand_Spearman extends Character {
     public Brigand_Spearman() {
         super("Brigand Spearman",new String[] {"Piercing","Fire"},10);
         weapon = new Spear(5,.3);
+        weapon.setWielder("Brigand Spearman");
     }
 
     @Override
