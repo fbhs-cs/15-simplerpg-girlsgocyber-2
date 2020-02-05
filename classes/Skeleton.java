8137a7b6e7052a391d2025bbf9465b606d0c2e63
kyletgithub <kyledthreatt@gmail.com>
2020-02-05 12:17:53 -0600

    corrected capitalization

--- classes/Skeleton.java
+++ classes/Skeleton.java
@@ -1,7 +1,7 @@
 public class Skeleton extends Character {
     private Weapon weapon;
     public Skeleton() {
-        super("Skeleton",new String[] {"blunt","holy"},10);
+        super("Skeleton",new String[] {"Blunt","Holy"},10);
         weapon = new Sword(6,.1);
     }
 
